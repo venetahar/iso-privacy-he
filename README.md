cba49db92c2718c537f1d8cd141659c97e2bdc41
Veneta Haralampieva <venetaharalampieva@Venetas-MacBook-Pro.local>
2020-04-30 15:28:40 +0100

    Update the model and the readme

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # iso-privacy-he
 
 Hosts the code necessary to run experiments on HE-Transformer. 
+
+First you need to have build the HE-Transformer: https://github.com/IntelAI/he-transformer/
+
+To run the HE-Transformer experiments do the following steps:
+1. Copy the iso-privacy-he directory to $HE_TRANSFORMER_INSTALLATION_DIR$/examples/
+```cp -R iso-privacy-he $HE_TRANSFORMER_INSTALLATION_DIR$/examples/```
+2. In one tab execute the following: 
+```cd $HE_TRANSFORMER_INSTALLATION_DIR$```
+```export HE_TRANSFORMER=`pwd` ```
+```cd build```
+```source external/venv-tf-py3/bin/activate```
+```cd $HE_TRANSFORMER/examples/iso-privacy-he```
+3. Open another tab and do:
+```cd $HE_TRANSFORMER_INSTALLATION_DIR$```
+```cd build```
+```source external/venv-tf-py3/bin/activate```
+```cd $HE_TRANSFORMER/examples/iso-privacy-he```
+4. To run MNIST in tab one execute: 
+```python3 mnist_server.py --model_file=mnist/models/alice_pool_model.pb --enable_client=true --encryption_parameters=$HE_TRANSFORMER/configs/he_seal_ckks_config_N13_L8.json ```
+5. In tab two execute: 
+```python3 mnist_client.py --batch_size=1 --encrypt_data_str=encrypt```
+
+You can do the same with the malaria experiments just replace mnist_server with malaria_server, 
+mnist_client with malaria_client and put the alice_conv_pool_model.pb as the model name.
--- train.py
+++ train.py
@@ -14,10 +14,21 @@ from common.model_factory import CONV_MODEL_TYPE, FULLY_CONNECTED_MODEL_TYPE
 from mnist.mnist_training import train_mnist_model
 
 
-train_mnist_model(FULLY_CONNECTED_MODEL_TYPE, MNIST_MODEL_PATH, MNIST_FULLY_CONNECTED_MODEL_NAME,
-                  MNIST_TARGET_DATA_PREFIX)
-train_mnist_model(CONV_MODEL_TYPE, MNIST_MODEL_PATH, MNIST_CONV_MODEL_NAME,
-                  MNIST_TARGET_DATA_PREFIX)
-train_malaria_model(model_path=MALARIA_MODEL_PATH, model_name=MALARIA_MODEL_NAME,
-                    source_data_path=MALARIA_DATA_PATH,
-                    target_data_path_prefix=MALARIA_TARGET_DATA_PATH_PREFIX)
+def train_model(model_type):
+    """
+    Trains a model.
+    :param model_type: The model type.
+    """
+    if model_type == 'mnist_fc':
+        train_mnist_model(FULLY_CONNECTED_MODEL_TYPE, MNIST_MODEL_PATH, MNIST_FULLY_CONNECTED_MODEL_NAME,
+                          MNIST_TARGET_DATA_PREFIX)
+    elif model_type == 'mnist_conv':
+        train_mnist_model(CONV_MODEL_TYPE, MNIST_MODEL_PATH, MNIST_CONV_MODEL_NAME,
+                          MNIST_TARGET_DATA_PREFIX)
+    elif model_type == 'malaria_conv':
+        train_malaria_model(model_path=MALARIA_MODEL_PATH, model_name=MALARIA_MODEL_NAME,
+                            source_data_path=MALARIA_DATA_PATH,
+                            target_data_path_prefix=MALARIA_TARGET_DATA_PATH_PREFIX)
+
+
+train_model('mnist_conv')
